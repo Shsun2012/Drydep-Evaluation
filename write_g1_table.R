f85d369c7191e21b86376c048918f8b82a591baa
shsun <s.susie123@gmail.com>
2019-12-30 14:39:00 +0800

    Create write_g1_table.R

---
+++ write_g1_table.R
@@ -0,0 +1,34 @@
+### PFT stomatal conductance parameters in CLM5.0
+# 
+#       PFT           | g1
+# 1  NET Temperate     |2.35
+# 2  NET Boreal        |2.35
+# 3  NDT Boreal        |2.35
+# 4  BET Tropical      |4.12
+# 5  BET temperal      |4.12
+# 6  BDT Tropical      |4.45
+# 7  BDT temperate     |4.45
+# 8  BDT boreal        |4.45
+# 9  BES temperate     |4.7
+# 10 BDS temperate     |4.7
+# 11 BDS boreal        |4.7
+# 12 C3 arctic grass   |2.22
+# 13 C3 grass          |5.25
+# 14 C4 grass          |1.62
+# 15 temperate corn    |1.79
+# 16 spring wheat      |5.79
+# 17 temperate soybean |5.79
+# 18 cotton            |5.79
+# 19 rice              |5.79
+# 20 sugarcane         |1.79
+# 21 tropical corn     |1.79
+# 22 tropical soybean  |5.79
+
+
+g1_val = c(2.35,2.35,2.35,4.12,4.12,4.45,4.45,4.45,4.70,4.70,4.70,2.22,5.25,1.62,1.79,5.79,5.79,5.79,5.79,1.79,1.79,5.79)
+mapping_TEMIR = c(NA,1:14)
+
+
+
+
+
